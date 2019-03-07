ed55c3d03e44bcd2707b3ba1c4d01ddcbdf1c712
Ivan Sim <ivan@buoyant.io>
2019-03-06 21:35:11 -0800

    Unit tests for image pull policy annotations
    Signed-off-by: Ivan Sim <ivan@buoyant.io>

--- pkg/k8s/labels.go
+++ pkg/k8s/labels.go
@@ -224,7 +224,9 @@ var (
 	// proxy configurations
 	ProxyConfigAnnotations = []string{
 		ProxyImageAnnotation,
+		ProxyImagePullPolicyAnnotation,
 		ProxyInitImageAnnotation,
+		ProxyInitImagePullPolicyAnnotation,
 		ProxyControlPortAnnotation,
 		ProxyIgnoreInboundPortsAnnotation,
 		ProxyIgnoreOutboundPortsAnnotation,
